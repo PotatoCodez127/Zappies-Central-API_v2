09e6827506fded0c9b2a1ab7197dc01e0ee3f86a
jeandre <jeandre.c127@gmail.com>
2026-04-15 17:31:47 +0200

    debug

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 import uvicorn
 import os
 
+print("DEBUG - Supabase URL is:", settings.SUPABASE_URL)
+print("DEBUG - History Table is:", settings.DB_CONVERSATION_HISTORY_TABLE)
+
 if __name__ == "__main__":
     # Get the port from the environment variable, default to 8000
     # This is important for deployment services like Heroku
